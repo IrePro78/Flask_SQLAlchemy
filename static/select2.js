905cca0fc500de3d058767a036afdee208472f7d
IrePro78 <gos3r78@gmail.com>
2021-09-15 14:47:11 +0200

    add_to_select2_each()

--- static/select2.js
+++ static/select2.js
@@ -1,6 +1,10 @@
 
+
 $(document).ready(function () {
-    $('#selAuthor').select2({
+
+  $("select[id^='selA']").each(function() {
+        console.log(this);
+    $(this).select2({
         language: "pl",
         tags: true,
         ajax: {
@@ -18,64 +22,15 @@ $(document).ready(function () {
         },
         minimumInputLength: 2
     });
-});
-
-
-
-
-
-$(document).ready(function () {
-    $('#selPublisher').select2({
-        language: "pl",
-        tags: true,
-        ajax: {
-            url: '/publishers',
-            type: 'post',
-            dataType: 'json',
-            delay: 250,
-            processResults: function (data) {
-                return {
-                    results: $.map(data, function (item) {
-                        return {id: item.id, text: item.name};
-                    })
-                };
-            }
-        },
-        minimumInputLength: 2
-    });
-});
-
-
-
-$(document).ready(function () {
-
-  $("select[id^='sel']").each(function(index, element) {
-
-    $(element).select2({
-        language: "pl",
-        tags: true,
-        ajax: {
-            url: '/authors',
-            type: 'post',
-            dataType: 'json',
-            delay: 250,
-            processResults: function (data) {
-                return {
-                    results: $.map(data, function (item) {
-                        return {id: item.id, text: item.name};
-                    })
-                };
-            }
-        },
-        minimumInputLength: 2
-    });
-});
+ });
 });
 
 
 $(document).ready(function () {
-    $('#selPublisher1').select2({
 
+  $("select[id^='selP']").each(function() {
+        console.log(this);
+    $(this).select2({
         language: "pl",
         tags: true,
         ajax: {
@@ -94,4 +49,6 @@ $(document).ready(function () {
         },
         minimumInputLength: 2
     });
+ });
 });
+
