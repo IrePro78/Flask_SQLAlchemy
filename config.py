e9961926ca54a71566f68b1335d01de94bf739e4
IrePro78 <78473540+IrePro78@users.noreply.github.com>
2021-09-26 14:10:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'flaskdjangopython@gmail.com'
-app.config['MAIL_PASSWORD'] = '***REMOVED***'
+app.config['MAIL_PASSWORD'] = '*********'
 app.config['MAIL_DEFAULT_SENDER'] = 'admin@gmail.pl'
